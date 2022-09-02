ae607bf1fcb69ebf2387a33e020d932867dea09b
AnjaHH <anja2590@easv365.dk>
2022-09-02 10:46:21 +0200

    2 commit

---
+++ single-mal-livet-pa-land.php
@@ -0,0 +1,52 @@
+
+<div class="container">
+
+    <div class="intro">
+        <div class="heading">MÅL 15: LIVET PÅ LANDET</div>
+        <div class="row">
+            <div class="col-4">billede</div>
+            <div class="col-8">
+                <h3><?php the_field("intro-text") ?></h3>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="description">
+        <div>
+            <p><?php the_field("description-1") ?></p>
+        </div>
+        <div>
+            <p><?php the_field("description-2") ?></p>
+        </div>
+
+    </div>
+
+    <div class="video">
+
+    </div>
+
+    <div class="second-description">
+        <div class="description-3">
+            <p><?php the_field("description-3") ?></p>
+            <p><?php the_field("description-4") ?></p>
+        </div>
+            <div class="row">
+                <div class="col"><?php the_field("aboutmetext3") ?></div>
+                <div class="col"><?php the_field("aboutmetext3") ?></div>
+            </div>
+    </div>
+
+    <div class="small-goals">
+
+    </div>
+
+    <div class="image">
+        <img class="image" src="<?php the_field("image") ?>" alt="">
+    </div>
+
+    <div class="all-goals">
+
+    </div>
+
+</div>
