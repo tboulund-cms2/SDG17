9541fd10344669301dbf78cad4a7b2a5ca2a4284
jakub-stelzer <jakub.stelzer1@gmail.com>
2022-09-02 16:03:30 +0200

    first bigger update

--- single-goal-life-below-water.php
+++ single-goal-life-below-water.php
@@ -3,7 +3,23 @@
     <?php while(have_posts()): the_post() ?>
 
     <section class="body">
-            <h1>Body</h1>
+    <div class="row">
+         <div class="top-headline col-11">
+            <div class="col-2">
+                <img class="headline-image" src="<?php the_field("headline_image") ?>" alt="">
+            </div>
+
+            <div class="col-2"></div>
+
+            <div class="headline-text col-6">
+                <p><?php the_field("headline_text") ?></p>
+            </div>
+            
+            <div class="col-2"></div>        
+                    
+        </div>
+        <div class="col-1"></div>
+    </div>
         </section>
 
     <?php endwhile ?>
