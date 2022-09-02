33c2375a7a7385f97f63714569b29c7f3c994261
jakub-stelzer <jakub.stelzer1@gmail.com>
2022-09-02 16:16:01 +0200

    Update single-goal-life-below-water.php

--- single-goal-life-below-water.php
+++ single-goal-life-below-water.php
@@ -2,27 +2,44 @@
 
     <?php while(have_posts()): the_post() ?>
 
-    <section class="body">
+    
         <div class="container">
-    <div class="row">
-         <div class="top-headline col-11">
-            <div class="col-2">
-                <img class="headline-image" src="<?php the_field("headline_image") ?>" alt="">
-            </div>
+            <div class="row">
+                <section class="part_1">
+                    <div class="top-headline col-11">
+                        <div class="col-2">
+                            <img class="headline-image" src="<?php the_field("headline_image") ?>" alt="">
+                        </div>
 
-            <div class="col-2"></div>
+                        <div class="col-2"></div>
 
-            <div class="headline-text col-6">
-                <p><?php the_field("headline_text") ?></p>
+                        <div class="headline-text col-6">
+                            <p><?php the_field("headline_text") ?></p>
+                        </div>
+                        
+                        <div class="col-2"></div>        
+                                
+                    </div>
+                    <div class="col-1"></div>
+                </section>
+                
+                <section class="part_2">
+                    <div class="col-12">
+                        <p><?php the_field("quote") ?></p>
+                    </div>
+                    <div class="col-12">
+                        <p><?php the_field("guote_author") ?></p>
+                    </div>
+                </section>
+
+                <section class="part_3">
+                    <div class="col-12">
+                            <img class="informative-image" src="<?php the_field("informative_image") ?>" alt="">
+                    </div>
+                </section>
             </div>
-            
-            <div class="col-2"></div>        
-                    
-        </div>
-        <div class="col-1"></div>
-    </div>
-    </div>
-        </section>
+      </div>
+        
 
     <?php endwhile ?>
     
