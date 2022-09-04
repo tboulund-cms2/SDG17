0df69d08b43732f97ae2d6a23e2df39b8abc14c4
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-04 15:45:30 +0200

    Update single-goal-good-health-and-well-being.php
    display Action Title

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -1,23 +1,21 @@
 <?php get_header()?>
-
+<?php while(have_posts()): the_post() ?>
 
 
 
 
 <!-- Actions Section - Rose -->
 
-<?php while(have_posts()): the_post() ?>
 
-        <!-- <section class="banner">
-            <h1>Bæredygtig energi</h1>
-        </section>
-        <section class="info1"></section>
-        <section class="info2"></section>
-        <section class="info3"></section>
-        <section class="goals"></section> -->
+    <h2>
+        <?php 
+            the_field("action_title")
+        ?>
+    </h2>
 
- <?php endwhile ?>
+
 
  <!-- End of Actions Section - Rose -->
+ <?php endwhile ?>
 
 <?php get_footer(); ?>
