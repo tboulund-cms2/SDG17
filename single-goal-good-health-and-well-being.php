b3441d5cb9cadb53f53cb918e68b2e56e4082435
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-04 16:31:54 +0200

    display item 2 group

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -14,8 +14,8 @@
                 <?php the_field("action_description") ?>
             </p>
         </div>
-        <div class="row action-items-section">
-            <div class="item-group-1">
+        <div class="row action-items-section d-flex flex-wrap">
+            <div class="item-group-1 mb-5">
                 <div class="item-image">
                     <?php echo wp_get_attachment_image(337, array('674', '448')); ?>
                 </div>  
@@ -24,6 +24,17 @@
                 </h3>
                 <p>
                     <?php the_field("action_item_description") ?>
+                </p>
+            </div>
+            <div class="item-group-2">
+                <div class="item-image">
+                    <?php echo wp_get_attachment_image(338, array('674', '448')); ?>
+                </div>  
+                <h3>
+                    <?php the_field("action_item_title_2") ?>
+                </h3>
+                <p>
+                    <?php the_field("action_item_description_2") ?>
                 </p>
             </div>
         </div>
