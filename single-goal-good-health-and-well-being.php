ae9e99bfc5fd0824da4ad88768d8026c9faf3764
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-04 15:52:11 +0200

    adding color to background
    adding background good health and well-being color css

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -1,21 +1,21 @@
 <?php get_header()?>
-<?php while(have_posts()): the_post() ?>
+    <?php while(have_posts()): the_post() ?>
 
 
 
 
-<!-- Actions Section - Rose -->
+    <!-- Actions Section - Rose -->
 
 
-    <h2>
-        <?php 
-            the_field("action_title")
-        ?>
-    </h2>
+        <h2>
+            <?php 
+                the_field("action_title")
+            ?>
+        </h2>
 
 
 
- <!-- End of Actions Section - Rose -->
- <?php endwhile ?>
+    <!-- End of Actions Section - Rose -->
+    <?php endwhile ?>
 
 <?php get_footer(); ?>
