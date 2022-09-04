127d2342bc3a2c6ba1bd46aae151d380bb4198e4
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-04 17:05:42 +0200

    added col-xl breakpoint

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -15,7 +15,7 @@
             </p>
         </div>
         <div class="row action-items-section d-flex flex-row flex-wrap ">
-            <div class="item-group-1 mb-5 col-6">
+            <div class="item-group-1 mb-5 col-12 col-xl-6">
                 <div class="item-image item-image-1">
                     <?php echo wp_get_attachment_image(337, array('565', '375.55')); ?>
                 </div>  
@@ -26,7 +26,7 @@
                     <?php the_field("action_item_description") ?>
                 </p>
             </div>
-            <div class="item-group-2 mb-5 col-6">
+            <div class="item-group-2 mb-5 col-12 col-xl-6">
                 <div class="item-image item-image-2">
                     <?php echo wp_get_attachment_image(338, array('565', '375.55')); ?>
                 </div>  
@@ -37,7 +37,7 @@
                     <?php the_field("action_item_description_2") ?>
                 </p>
             </div>
-            <div class="item-group-3 mb-5 col-6">
+            <div class="item-group-3 mb-5 col-12 col-xl-6">
                 <div class="item-image item-image-3">
                     <?php echo wp_get_attachment_image(339, array('565', '375.55')); ?>
                 </div>  
@@ -48,7 +48,7 @@
                     <?php the_field("action_item_description_3") ?>
                 </p>
             </div>
-            <div class="item-group-4 col-6">
+            <div class="item-group-4 col-12 col-xl-6">
                 <div class="item-image item-image-4">
                     <?php echo wp_get_attachment_image(340, array('565', '375.55')); ?>
                 </div>  
