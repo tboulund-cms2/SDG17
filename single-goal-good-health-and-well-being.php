53ea671b5cb42679587860f7f82030c183d79280
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-04 17:01:16 +0200

    added column wrapping

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -15,9 +15,9 @@
             </p>
         </div>
         <div class="row action-items-section d-flex flex-row flex-wrap ">
-            <div class="item-group-1 mb-5">
+            <div class="item-group-1 mb-5 col-6">
                 <div class="item-image item-image-1">
-                    <?php echo wp_get_attachment_image(337, array('420', '280')); ?>
+                    <?php echo wp_get_attachment_image(337, array('565', '375.55')); ?>
                 </div>  
                 <h3 class="my-2">
                     <?php the_field("action_item_title_1") ?>
@@ -26,9 +26,9 @@
                     <?php the_field("action_item_description") ?>
                 </p>
             </div>
-            <div class="item-group-2 mb-5">
+            <div class="item-group-2 mb-5 col-6">
                 <div class="item-image item-image-2">
-                    <?php echo wp_get_attachment_image(338, array('420', '280')); ?>
+                    <?php echo wp_get_attachment_image(338, array('565', '375.55')); ?>
                 </div>  
                 <h3 class="my-2">
                     <?php the_field("action_item_title_2") ?>
@@ -37,9 +37,9 @@
                     <?php the_field("action_item_description_2") ?>
                 </p>
             </div>
-            <div class="item-group-3 mb-5">
+            <div class="item-group-3 mb-5 col-6">
                 <div class="item-image item-image-3">
-                    <?php echo wp_get_attachment_image(339, array('420', '280')); ?>
+                    <?php echo wp_get_attachment_image(339, array('565', '375.55')); ?>
                 </div>  
                 <h3 class="my-2">
                     <?php the_field("action_item_title_3") ?>
@@ -48,9 +48,9 @@
                     <?php the_field("action_item_description_3") ?>
                 </p>
             </div>
-            <div class="item-group-4">
+            <div class="item-group-4 col-6">
                 <div class="item-image item-image-4">
-                    <?php echo wp_get_attachment_image(340, array('420', '280')); ?>
+                    <?php echo wp_get_attachment_image(340, array('565', '375.55')); ?>
                 </div>  
                 <h3 class="my-2">
                     <?php the_field("action_item_title_4") ?>
