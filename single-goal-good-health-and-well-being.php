eb04bbe19d494cc4bf0f0bb9661a667a41142297
JaneyFan <jian0177@easv365.dk>
2022-09-05 23:32:18 +0200

    hero changes

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -13,11 +13,14 @@
     <div class="hero-section-container"> 
         <div class="row hero-section">
             <div class="hero-image col-12">
-                <?php echo wp_get_attachment_image(389, array('1920', '931')); ?>                                
-                <div class="d-flex flex-column">
-                    <h1 class="hero-title align-self-center"><?php the_field("hero_title") ?></h1>
-                    <h3 class="hero-subtitle align-self-center"><?php the_field("hero_subtitle") ?></h3>
+                <?php echo wp_get_attachment_image(389, array('1920', '931')); ?>   
+                <div class="row">                             
+                    <div class="d-flex flex-column">
+                        <h1 class="hero-title align-self-center"><?php the_field("hero_title") ?></h1>
+                        <h3 class="hero-subtitle align-self-center"><?php the_field("hero_subtitle") ?></h3>
+                    </div>
                 </div>
+                
             </div>
         </div>
     </div>
