cec9f683f51bd4a6ee8bb46409ca6bc6d2b421f8
mich163w <71692337+mich163w@users.noreply.github.com>
2022-09-05 13:20:35 +0200

    tekst

--- single-mal-stop-sult.php
+++ single-mal-stop-sult.php
@@ -8,18 +8,18 @@
 
     <div class="first">
       
-       <p><?php the_field("text") ?></p> 
+       <p class="tekst"><?php the_field("text") ?></p> 
         <img class="image" src="<?php the_field("image") ?>" alt="">
     </div>
 
     <div class="second">
     <img class="image" src="<?php the_field("image-2") ?>" alt="">
-    <p><?php the_field("text-2") ?></p> 
+    <p class="tekst"><?php the_field("text-2") ?></p> 
        
     </div>
 
     <div class="third">
-    <p><?php the_field("text-3") ?></p> 
+    <p class="tekst"><?php the_field("text-3") ?></p> 
         <img class="image" src="<?php the_field("image-3") ?>" alt="">
     </div> 
     </div>
