06a2e2e5a4e653147ab527e216b36e811b78bce2
jakub-stelzer <jakub.stelzer1@gmail.com>
2022-09-05 19:16:49 +0200

    targets2

--- single-goal-life-below-water.php
+++ single-goal-life-below-water.php
@@ -1,5 +1,5 @@
-<?php get_header()?>
-    <?php while(have_posts()): the_post() ?>
+<?php get_header() ?>
+<?php while (have_posts()) : the_post() ?>
 
 
 
@@ -9,88 +9,90 @@
     <section id="headline-section" class="col-12">
         <div class="col-12 headline-box">
             <div class="col-1"></div>
-                <img src="<?php the_field("headline_image") ?>" alt="headline" class="col-2">
+            <img src="<?php the_field("headline_image") ?>" alt="headline" class="col-2">
 
-                <div class="col-9 headline-text">
-                    <div class="col-2"></div>
-                    
-                    <div class="col-8 text-color">
-                        <h1><?php the_field("headline_text") ?></h1>
-                    </div>
-                    <div class="col-2"></div>
+            <div class="col-9 headline-text">
+                <div class="col-2"></div>
+
+                <div class="col-8 text-color">
+                    <h1><?php the_field("headline_text") ?></h1>
+                </div>
+                <div class="col-2"></div>
             </div>
         </div>
     </section>
 
-               
-                
-                <section id="quote-section" class="col-12">
-                    <div class="col-12 quote-up">
-                            <div class="col-3"></div>
-                            <div class="col-6 text-color">
-                                <h2><?php the_field("quote") ?></h2>
-                            </div>
-                            <div class="col-3"></div>
-                    </div>
-                        
-                    <div class="col-12">
-                        <div class="col-12 quote-down">
-                                <div class="col-4"></div>
-                                <div class="col-4 text-color">
-                                    <h2><?php the_field("quote_author") ?></h2>
-                                </div>
-                                <div class="col-4"></div>
+
+
+    <section id="quote-section" class="col-12">
+        <div class="col-12 quote-up">
+            <div class="col-3"></div>
+            <div class="col-6 text-color">
+                <h2><?php the_field("quote") ?></h2>
+            </div>
+            <div class="col-3"></div>
+        </div>
+
+        <div class="col-12">
+            <div class="col-12 quote-down">
+                <div class="col-4"></div>
+                <div class="col-4 text-color">
+                    <h2><?php the_field("quote_author") ?></h2>
+                </div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+    </section>
+
+    <section id="informative-section">
+        <img class="informative-image" src="<?php the_field("informative_image") ?>" alt="">
+        </div>
+    </section>
+
+
+    <section id="targets-section" class="col-12">
+        <div class="col-12">
+            <h2><?php the_field("headline") ?></h2>
+        </div>
+        <div class="col-12 target-container">
+            <div class="col-6 target-one-left">
+                <div class="col-4">
+                    <img class="target-image" src="<?php the_field("target_image") ?>" alt="">
+                </div>
+                <div class="col-8">
+                    <div class="col"></div>
+                    <div class="col-10 target-box">
+                        <div class="col-12 target-box-headline">
+                            <?php the_field("target_headline") ?>
+                        </div>
+                        <div class="col-12 target-box-description">
+                            <?php the_field("target_description") ?>
                         </div>
                     </div>
-                </section>
+                    <div class="col"></div>
+                </div>
+            </div>
 
-                <section id="informative-section">
-                            <img class="informative-image" src="<?php the_field("informative_image") ?>" alt="">
+            <div class="col-6 target-one-right">
+                <div class="col-4">
+                    <img class="target-image" src="<?php the_field("target_image_2") ?>" alt="">
+                </div>
+                <div class="col-8">
+                    <div class="col"></div>
+                    <div class="col-10 target-box">
+                        <div class="col-12 target-box-headline">
+                            <?php the_field("target_headline_2") ?>
+                        </div>
+                        <div class="col-12 target-box-description">
+                            <?php the_field("target_description_2") ?>
+                        </div>
                     </div>
-                </section>
-        
+                    <div class="col"></div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-                <section id="targets-section" class="col-12">
-                    <div class="col-12">
-                         <h2><?php the_field("headline") ?></h2>
-                    </div>
-                    <div class="col-6 target-one-left">
-                            <div class="col-4">
-                                <img class="target-image" src="<?php the_field("target_image") ?>" alt="">
-                            </div>
-                            <div class="col-8">
-                                <div class="col"></div>
-                                <div class="col-10 target-box">
-                                    <div class="col-12 target-box-headline">
-                                        <?php the_field("target_headline") ?>
-                                    </div>
-                                    <div class="col-12 target-box-description">
-                                        <?php the_field("target_description") ?>  
-                                    </div>
-                                </div>
-                                <div class="col"></div>
-                            </div>
-                    </div>
+<?php endwhile ?>
 
-                    <div class="col-6 target-one-right">
-                            <div class="col-4">
-                                <img class="target-image" src="<?php the_field("target_image_2") ?>" alt="">
-                            </div>
-                            <div class="col-8">
-                                <div class="col"></div>
-                                <div class="col-10 target-box">
-                                    <div class="col-12 target-box-headline">
-                                        <?php the_field("target_headline_2") ?>
-                                    </div>
-                                    <div class="col-12 target-box-description">
-                                        <?php the_field("target_description_2") ?>  
-                                    </div>
-                                </div>
-                                <div class="col"></div>
-                            </div>
-                    </div>
-                </section>
-
-    <?php endwhile ?>
-    
 <?php get_footer(); ?>
