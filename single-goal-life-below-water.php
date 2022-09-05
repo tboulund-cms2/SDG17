eb9ec8c6d28d8eeecc78a500168db2c4df8c65ee
jakub-stelzer <jakub.stelzer1@gmail.com>
2022-09-05 18:13:43 +0200

    3

--- single-goal-life-below-water.php
+++ single-goal-life-below-water.php
@@ -1,32 +1,25 @@
 <?php get_header()?>
     <?php while(have_posts()): the_post() ?>
 
-    <section id="headline-section" class="d-flex col-md-12 align-items-center justify-content-center info-background">
-        <div class="d-flex col-md-10 col-sm-12 flex-column flex-md-row justify-content-center align-items-center justify-content-md-start headline-container">
-            <img src="<?php the_field("headline_image") ?>" alt="banner" class="col-12 col-md-3">
-            <div class="d-flex col-12 col-md-9 col-xl-9 justify-content-center px-4 banner-text text-center text-md-start">
-             <h1><?php the_field("headline_text") ?></h1>
+
+
+
+
+
+    <section id="headline-section" class="col-12">
+        <div class="col-12 headline-box">
+
+            <img src="<?php the_field("headline_image") ?>" alt="headline" class="col-2">
+
+            <div class="col-10">
+
+                 <h1><?php the_field("headline_text") ?></h1>
+
             </div>
         </div>
     </section>
 
-                <section class="part_1">
-                    <div class="top-headline col-11">
-                        <div class="col-2">
-                            <img src="<?php the_field("headline_image") ?>" alt="">
-                        </div>
-
-                        <div class="col-2"></div>
-
-                        <div class="headline-text col-6">
-                            
-                        </div>
-                        
-                        <div class="col-2"></div>        
-                                
-                    </div>
-                    <div class="col-1"></div>
-                </section>
+               
                 
                 <section class="part_2">
                     <div class="col-12">
