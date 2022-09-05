92fbf7f15c852168e6289d0842b67651f2d7fcad
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-05 10:35:03 +0200

    added flex column

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -47,7 +47,7 @@
     <!-- Actions Section - Rose -->
     <div class="action-section container">
         <div class="row action-title-section my-5">
-            <div class="col-12 d-flex justify-content-center align-items-center">
+            <div class="col-12 d-flex justify-content-center align-items-cente flex-column">
                     <h2>
                         <?php the_field("action_title") ?>
                     </h2>
