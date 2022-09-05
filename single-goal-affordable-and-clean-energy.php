4df8619d2a40610817cda65e63e3ddb3883b7066
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-09-05 09:45:24 +0200

    Text to news

--- single-goal-affordable-and-clean-energy.php
+++ single-goal-affordable-and-clean-energy.php
@@ -4,7 +4,7 @@
     <section id="banner-section" class="d-flex col-md-12 align-items-center justify-content-center info-background">
         <div class="d-flex col-md-8 col-sm-12 flex-column flex-md-row justify-content-center align-items-center justify-content-md-start banner-container">
             <img src="" alt="" class="col-12 col-md-3">
-            <div class="d-flex col-12 col-md-8 col-xl-5 justify-content-center px-4 banner-text text-center text-md-start">
+            <div class="d-flex col-12 col-md-9 col-xl-5 justify-content-center px-4 banner-text text-center text-md-start">
                 <h1 id="banner-description" class="col-8 col-md-12">Ensure access to affordable, reliable, sustainable and modern energy for all.</h1>
             </div>
         </div>
@@ -73,24 +73,21 @@
                     <img src="" class="card-img-top" alt="...">
                     <!-- <img class="card-img-top" src="<?php the_field("news_image_1") ?>" alt="news1"> -->
                     <div class="card-body">
-                        <!-- <p class="col-6"><?php the_field("news_description") ?> </p> -->
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"><?php the_field("highlighted_news_article_1") ?> </p>
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
                     <img src="" class="card-img-top" alt="...">
                     <!-- <img class="card-img-top" src="<?php the_field("news_image_2") ?>" alt="news2"> -->
                     <div class="card-body">
-                        <!-- <p class="col-6"><?php the_field("news_description") ?> </p> -->
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"><?php the_field("highlighted_news_article_2") ?> </p>
                     </div>
                 </div>
                 <div class="card col-12 col-md-3">
                     <img src="" class="card-img-top" alt="...">
                     <!-- <img class="card-img-top" src="<?php the_field("news_image_3") ?>" alt="news3"> -->
                     <div class="card-body">
-                        <p class="card-text"><?php the_field("news_description") ?> </p>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"><?php the_field("highlighted_news_article_3") ?> </p>
                     </div>
                 </div>
             </div>
