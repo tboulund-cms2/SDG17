e53473135d0bcf45ef0c8cac2a04ec330c40d738
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-05 10:43:30 +0200

    change in action section

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -59,7 +59,7 @@
     <div class="action-section container">
         <div class="row action-title-section my-5">
             <div class="col-12 d-flex justify-content-center align-items-center flex-column flex-wrap">
-                <div class="action-title-container col-6">
+                <div class="action-title-container col-6 justify-content-center align-items-center">
                     <h2>
                         <?php the_field("action_title") ?>
                     </h2>
@@ -67,7 +67,6 @@
                         <?php the_field("action_description") ?>
                     </p>
                 </div>
-                   
             </div>
         </div>
         <div class="row action-items-section d-flex flex-row flex-wrap ">
