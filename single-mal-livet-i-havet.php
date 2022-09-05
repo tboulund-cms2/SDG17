dfeb8f93450a43f1e15a377137e062d6a469351c
SigneThrane <signecathrane@gmail.com>
2022-09-05 15:20:47 +0200

    Update single-mal-livet-i-havet.php

--- single-mal-livet-i-havet.php
+++ single-mal-livet-i-havet.php
@@ -46,26 +46,26 @@
                         <br>
 
                        <span id="yellowtekst">By 2020</span>, sustainably manage and protect marine and coastal ecosystems to avoid significant adverse impacts, including by strengthening their resilience, and take action for their restoration in order to achieve healthy and productive oceans <br>
-
+                       <br>
 
                         <span id="yellowtekst">Minimize and address</span> the impacts of ocean acidification, including through enhanced scientific cooperation at all levels <br>
-
+                        <br>
 
                        <span id="yellowtekst">By 2020</span>, effectively regulate harvesting and end overfishing, illegal, unreported and unregulated fishing and destructive fishing practices and implement science-based management plans, in order to restore fish stocks in the shortest time feasible, at least to levels that can produce maximum sustainable yield as determined by their biological characteristics <br>
-
+                       <br>
 
                         <span id="yellowtekst">By 2020</span>, conserve at least 10 per cent of coastal and marine areas, consistent with national and international law and based on the best available scientific information <br>
-
+                        <br>
 
                        <span id="yellowtekst">By 2020</span>, prohibit certain forms of fisheries subsidies which contribute to overcapacity and overfishing, eliminate subsidies that contribute to illegal, unreported and unregulated fishing and refrain from introducing new such subsidies, recognizing that appropriate and effective special and differential treatment for developing and least developed countries should be an integral part of the World Trade Organization fisheries subsidies negotiation <br>
-
+                       <br>
 
                          <span id="yellowtekst">By 2030</span>, increase the economic benefits to Small Island developing States and least developed countries from the sustainable use of marine resources, including through sustainable management of fisheries, aquaculture and tourism <br>
                         Increase scientific knowledge, develop research capacity and transfer marine technology, taking into account the Intergovernmental Oceanographic Commission Criteria and Guidelines on the Transfer of Marine Technology, in order to improve ocean health and to enhance the contribution of marine biodiversity to the development of developing countries, in particular small island developing States and least developed countries <br>
-
+                        <br>
 
                         <span id="yellowtekst">Provide access</span> for small-scale artisanal fishers to marine resources and markets <br>
-
+                        <br>
 
                         <span id="yellowtekst">Enhance</span> Enhance the conservation and sustainable use of oceans and their resources by implementing international law as reflected in UNCLOS, which provides the legal framework for the conservation and sustainable use of oceans and their resources, as recalled in paragraph 158 of The Future We Want
                     </p>
