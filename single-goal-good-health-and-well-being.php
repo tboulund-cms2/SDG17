de8889c338ca2e7fca0e729e083efafba4c73c45
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-05 10:37:07 +0200

    fixed align items center

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -47,7 +47,7 @@
     <!-- Actions Section - Rose -->
     <div class="action-section container">
         <div class="row action-title-section my-5">
-            <div class="col-6 d-flex justify-content-center align-items-cente flex-column flex-wrap">
+            <div class="col-6 d-flex justify-content-center align-items-center flex-column flex-wrap">
                     <h2>
                         <?php the_field("action_title") ?>
                     </h2>
