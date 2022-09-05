06f38c7a9347388d6776624d6f7638142761ffb9
SigneThrane <signecathrane@gmail.com>
2022-09-05 13:04:56 +0200

    tekst prøve

--- single-mal-livet-i-havet.php
+++ single-mal-livet-i-havet.php
@@ -6,6 +6,12 @@
     <img src="<?php the_field('hero_picture'); ?>" alt="" />
     </div>
 
+    <div>
+      <h1>14</h1>
+      <h2>Conserve and sustainably use the oceans, <br>
+seas and marine resources for sustainable development</h2> 
+    </div>
+
     <?php endwhile ?>
 
 <?php get_footer() ?>
