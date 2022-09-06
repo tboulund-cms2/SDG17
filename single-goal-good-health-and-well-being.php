6602c73b7a9e0dcf4c6c80521e211330333b00f5
JaneyFan <jian0177@easv365.dk>
2022-09-06 13:51:01 +0200

    navebar changes

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -4,7 +4,7 @@
     <!-- start by Jianming -->
     <div class="row navbar">        
             <div class="logo-box col-2 my-2">                
-                <div class="square-logo d-flex justify-content-center align-items-center">
+                <div class="square-logo">
                     <img src="<?php the_field("square_logo") ?>" alt="">
                 </div>
             </div>
