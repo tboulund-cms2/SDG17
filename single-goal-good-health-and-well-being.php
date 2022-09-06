a5abe514a68248b915e6a7da5e52fcd05af984b6
JaneyFan <jian0177@easv365.dk>
2022-09-06 16:44:16 +0200

    changed data section

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -18,8 +18,8 @@
                 </div>       
     </div>
 
-    <div class="data-section bg-primary mb-5 col-12 col-xl-6">
-            <div class="row data-group d-flex justify-content-center align-items-center">
+    <div class="data-section bg-primary d-flex justify-content-center align-items-center mb-5 col-12 col-xl-6">
+            <div class="row data-group ">
                 <div class="data-group-1">
                     <h1 class="data-number"><?php the_field("data_number_1") ?></h1>
                     <p class="data-description"><?php the_field("data_description_1") ?></p>
