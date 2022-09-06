fa00a8ca857b79b3a76904163ffbd5c1ad86c446
JaneyFan <jian0177@easv365.dk>
2022-09-06 20:48:57 +0200

    changed hero section

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -15,7 +15,7 @@
             <div class="row row-cols-1 hero-content bg-primary">
                 <!-- <div class="hero-title-section col-12 d-flex justify-content-center align-items-center flex-column flex-wrap"> -->                    
                     <h1 class="hero-title col my-2"><?php the_field("hero_title") ?></h1>
-                    <h5 class="hero-subtitle col"><?php the_field("hero_subtitle") ?></h5>
+                    <h5 class="hero-subtitle col my-2"><?php the_field("hero_subtitle") ?></h5>
                     <div class="call-to-action my-2 col">
                         <a class="btn btn-cta" role="button" href="http://boulund.com/sdg17/groups/good-health-and-well-being/forum/good-health-and-well-being/" target="_blank"> Join the Forum </a>
                     </div>
