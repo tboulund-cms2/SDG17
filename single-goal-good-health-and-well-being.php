c6aa9b94ecb23c3423827fdadbe3165dd7634d8e
Mary Rose Avenido <maryrose_avenido@yahoo.com>
2022-09-06 19:50:34 +0200

    Update single-goal-good-health-and-well-being.php

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -144,7 +144,7 @@
     <div class="action-section container">
         <div class="row action-title-section my-5">
             <div class="col-12 d-flex justify-content-center align-items-center flex-column flex-wrap">
-                <div class="action-title-container col-4 d-flex justify-content-center align-items-center flex-column flex-wrap">
+                <div class="action-title-container col-12 col-xl-6 d-flex justify-content-center align-items-center flex-column flex-wrap">
                     <h2>
                         <?php the_field("action_title") ?>
                     </h2>
