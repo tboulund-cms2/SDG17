c64aea49da4896e3066c7260112d7df765eb13f0
JaneyFan <jian0177@easv365.dk>
2022-09-06 16:28:53 +0200

    changed data section

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -17,6 +17,28 @@
                     <h5 class="hero-subtitle text-wrap"><?php the_field("hero_subtitle") ?></h5>
                 </div>       
     </div>
+
+    <div class="data-section mb-5 col-12 col-xl-6">
+            <div class="row data-group">
+                <div class="data-group-1">
+                    <h1 class="data-number-1"><?php the_field("data_number_1") ?></h1>
+                    <p class="data-description-1"><?php the_field("data_description_1") ?></p>
+                </div>  
+                <div class="data-group-2">
+                    <h1 class="data-number-2"><?php the_field("data_number_2") ?></h1>
+                    <p class="data-description-2"><?php the_field("data_description_2") ?></p>
+                </div> 
+                <div class="data-group-3">
+                    <h1 class="data-number-3"><?php the_field("data_number_3") ?></h1>
+                    <p class="data-description-3"><?php the_field("data_description_3") ?></p>
+                </div> 
+                <div class="data-group-4">
+                    <h1 class="data-number-4"><?php the_field("data_number_4") ?></h1>
+                    <p class="data-description-4"><?php the_field("data_description_4") ?></p>
+                </div>                
+                
+            </div>
+        </div>
     
     
     <!-- Targets Section - Leifur -->
