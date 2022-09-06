c87ef038e8340e0c5373097918cb5d4bf384511a
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-09-06 11:51:20 +0200

    Floating button adjustments

--- single-goal-affordable-and-clean-energy.php
+++ single-goal-affordable-and-clean-energy.php
@@ -97,7 +97,7 @@
         </div>
 
     </section>
-    <section id="floating-button-section">
+    <section id="floating-button-section d-none d-sm-inline">
         <a href="<?php the_field("affordable_and_clean_energy_group") ?>">
             <div class="floating-button d-flex justify-content-center align-items-center">
                 <p class="m-0 text-center"> Contribute <br> Now!</p>
