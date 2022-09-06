cd2c5703bf3450deef11a2127a2f9ac049af9e9b
JaneyFan <jian0177@easv365.dk>
2022-09-06 21:13:45 +0200

    changed hero section

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -10,17 +10,15 @@
         </div>
     </div>      
      
-    <div class="hero-section text-center">
-        <div class="hero-image-section">              
-            <img class="hero-image img-fluid" src="<?php the_field("hero_image") ?>" alt=""> 
-                <div class="hero-content bg-primary">                                  
-                    <h1 class="hero-title mt-2"><?php the_field("hero_title") ?></h1>
-                    <h5 class="hero-subtitle"><?php the_field("hero_subtitle") ?></h5>
-                    <div class="call-to-action my-2">
-                        <a class="btn btn-cta" role="button" href="http://boulund.com/sdg17/groups/good-health-and-well-being/forum/good-health-and-well-being/" target="_blank"> Join the Forum </a>
-                    </div>
-                </div> 
-        </div>     
+    <div class="hero-section text-center">              
+        <img class="hero-image img-fluid" src="<?php the_field("hero_image") ?>" alt=""> 
+            <div class="hero-content bg-primary">                                  
+                <h1 class="hero-title mt-2"><?php the_field("hero_title") ?></h1>
+                <h5 class="hero-subtitle"><?php the_field("hero_subtitle") ?></h5>
+                <div class="call-to-action my-2">
+                    <a class="btn btn-cta" role="button" href="http://boulund.com/sdg17/groups/good-health-and-well-being/forum/good-health-and-well-being/" target="_blank"> Join the Forum </a>
+                </div>
+            </div>      
     </div>
 
     <div class="data-section text-center my-3">
