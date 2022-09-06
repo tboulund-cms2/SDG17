2d6cefe248f18b92fa71a96866ba68e814848167
JaneyFan <jian0177@easv365.dk>
2022-09-06 19:02:50 +0200

    changed data section

--- single-goal-good-health-and-well-being.php
+++ single-goal-good-health-and-well-being.php
@@ -20,7 +20,7 @@
 
     <div class="data-section text-center my-3">
             <div class="row data-group mx-5 px-5 row-cols-1 row-cols-sm-2 row-cols-md-4">                
-                <div class="data-group-1 col">
+                <div class="data-group-1 col ml-5">
                     <h1 class="data-number"><?php the_field("data_number_1") ?></h1>
                     <p class="data-description"><?php the_field("data_description_1") ?></p>
                 </div>  
@@ -32,7 +32,7 @@
                     <h1 class="data-number"><?php the_field("data_number_3") ?></h1>
                     <p class="data-description"><?php the_field("data_description_3") ?></p>
                 </div> 
-                <div class="data-group-4 col">
+                <div class="data-group-4 col mr-5">
                     <h1 class="data-number"><?php the_field("data_number_4") ?></h1>
                     <p class="data-description"><?php the_field("data_description_4") ?></p>
                 </div>                
