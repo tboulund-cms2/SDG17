c5210cf486214b7d6465edb1b89598a6f28af510
xNans <nannamc@live.dk>
2022-09-07 13:56:21 +0200

    .

--- single-mal-baeredygtig-energi.php
+++ single-mal-baeredygtig-energi.php
@@ -6,22 +6,22 @@
             <div class="col-md-12 backgroundimg" style="background-image: url(<?php the_field("banner") ?>)">
                 <h1>Bæredygtig energi</h1>
                 <div class="col-md-12 btns">
-                    <a href="#goals" class="goal"><p>Mål for bæredygtig energi</p></a>
-                    <a href="/sdg17/groups/baeredygtig-energi/members/all-members/" class="forums"><p>Forums</p></a>
+                    <a href="#goals" class="goal"><p class="pconfiq">Mål for bæredygtig energi</p></a>
+                    <a href="/sdg17/groups/baeredygtig-energi/members/all-members/" class="forums"><p class="pconfiq">Forums</p></a>
                 </div>
             </div>
         </section>
         <section class="info1">
-            <h2 class="col-md-12 headertext1"><?php the_field("info1_overskrift")?></h2>
-            <p class="col-md-12 infotext1"><?php the_field("info1_text")?></p>
+            <h2 class="col-md-12 headertext1 h2confiq"><?php the_field("info1_overskrift")?></h2>
+            <p class="col-md-12 infotext1 pconfiq"><?php the_field("info1_text")?></p>
         </section>
         <section class="info2">
-            <h2 class="col-md-12 headertext1"><?php the_field("info2_overskrift")?></h2>
-            <p class="col-md-12 headertext1"><?php the_field("info2_text")?></p>
+            <h2 class="col-md-12 headertext1 h2confiq"><?php the_field("info2_overskrift")?></h2>
+            <p class="col-md-12 headertext1 pconfiq"><?php the_field("info2_text")?></p>
         </section>
         <section class="info3">
-            <h2 class="col-md-12 headertext1"><?php the_field("info3_overskrift")?></h2>
-            <p class="col-md-12 headertext1"><?php the_field("info3_text")?></p>
+            <h2 class="col-md-12 headertext1 h2confiq"><?php the_field("info3_overskrift")?></h2>
+            <p class="col-md-12 headertext1 pconfiq"><?php the_field("info3_text")?></p>
         </section>
 
         <div class="col-md-12 spacerContainer">
