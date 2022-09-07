4af04ffd578a42487a581a176dac49a69c011399
xNans <nannamc@live.dk>
2022-09-07 19:27:26 +0200

    .

--- single-mal-baeredygtig-energi.php
+++ single-mal-baeredygtig-energi.php
@@ -5,10 +5,6 @@
         <section class="banner">
             <div class="col-md-12 backgroundimg" style="background-image: url(<?php the_field("banner") ?>)">
                 <h1 class="titel1">Bæredygtig energi</h1>
-                <div class="logos">
-                    <div class="logo"></div>
-                    <div class="verdensmål"></div>
-                </div>
                 <div class="col-md-12 btns">
                     <a href="#goals" class="goal"><p class="pconfiq">Mål for bæredygtig energi</p></a>
                     <a href="/sdg17/groups/baeredygtig-energi/members/all-members/" class="forums"><p class="pconfiq">Forums</p></a>
@@ -17,8 +13,10 @@
         </section>
         <section class="info1">
             <div class="info1">
+                <div class="logo"></div>
                 <h2 class="col-md-12 headertext1 h2confiq"><?php the_field("info1_overskrift")?></h2>
                 <p class="col-md-12 infotext1 pconfiq"><?php the_field("info1_text")?></p>
+                <div class="verdensmål"></div>
             </div>
             
             
