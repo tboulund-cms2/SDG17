a65ef426a9c65551cf5d73bcad53ac1334f6f56b
Denni Knudsen <37368491+Deknudsen@users.noreply.github.com>
2022-09-07 15:31:04 +0200

    Update single-mal-baeredygtig-energi.php

--- single-mal-baeredygtig-energi.php
+++ single-mal-baeredygtig-energi.php
@@ -12,7 +12,7 @@
             </div>
         </section>
         <section>
-        
+        <iframe width="560" height="315" src="<?php the_field("video_link") ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </section>
         <section class="info1">
             <div class="info1">
