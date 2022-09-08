bedb239235267c3ed75ee649f0b8ba78bafeaa06
Bjarne666 <bjarne96@outlook.dk>
2022-09-08 13:18:25 +0200

    moved button

--- single-mal-baeredygtige-byer-og-lokalsamfund.php
+++ single-mal-baeredygtige-byer-og-lokalsamfund.php
@@ -40,17 +40,17 @@
     <div class="video">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/EHeWPWny3BE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-    <div class="col-md-12 btn">
-        <a href="http://boulund.com/sdg17/groups/baeredygtige-byer/members/all-members/" class="forums">
-            <p class="pcconfiq">Forum</p>
-        </a>
-    </div>
     <div class="description2">
         <div class="description-3">
             <p style="padding:50px,150px,20px,150px"><?php the_field("description-3") ?></p>
 
             <p style="padding:0px,150px,30px,150px"><?php the_field("description-4") ?></p>
         </div>
+    </div>
+    <div class="col-md-12 btn">
+        <a href="http://boulund.com/sdg17/groups/baeredygtige-byer/members/all-members/" class="forums">
+            <p class="pcconfiq">Forum</p>
+        </a>
     </div>
 
 
