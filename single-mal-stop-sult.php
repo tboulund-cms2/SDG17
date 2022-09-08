a35d75eae410a103448b35bee1917cf30038e422
mich163w <71692337+mich163w@users.noreply.github.com>
2022-09-08 14:24:28 +0200

    removed div

--- single-mal-stop-sult.php
+++ single-mal-stop-sult.php
@@ -32,14 +32,14 @@
     </div>
     </div>
  
-    <div class="col-2">
+  
     <button class="btn-b2"> Forum </button> 
     <a href="https://www.un.org/development/desa/disabilities/envision2030-goal2.html"> <button class="btn-b2">  Mål </button></a>
    
 
     <div class="video">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/2UK5mY8b8gg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
+
 </div>
     </div>
         
