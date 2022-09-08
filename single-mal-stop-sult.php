88369680fe132eadd6360efbb1e9b3154e99b208
mich163w <71692337+mich163w@users.noreply.github.com>
2022-09-08 11:44:51 +0200

    prøve

--- single-mal-stop-sult.php
+++ single-mal-stop-sult.php
@@ -9,28 +9,28 @@
     
 
 
-    <div class="first">
+    
       
        <p class="tekst"><?php the_field("text") ?></p> 
         <img class="image" src="<?php the_field("image") ?>" alt="">
-    </div>
+   
 
-    <div class="second">
+
     <img class="image-2" src="<?php the_field("image-2") ?>" alt="">
     <p class="tekst"><?php the_field("text-2") ?></p> 
        
-    </div>
+    
     </div>
     <div class="col">
-    <div class="third">
+   
     <p class="tekst"><?php the_field("text-3") ?></p> 
         <img class="image" src="<?php the_field("image-3") ?>" alt="">
     </div> 
 
-    <div class="fourth">
+   
     <img class="image" src="<?php the_field("image-4") ?>" alt="">
     <p class="tekst"><?php the_field("text-4") ?></p> 
-    </div>
+   
     </div>
     <button> Forum </button>
     
