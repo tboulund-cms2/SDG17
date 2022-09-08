2adfd0a1c4e31bf10490a4af395a0c4b4d6237ce
mich163w <71692337+mich163w@users.noreply.github.com>
2022-09-08 12:56:20 +0200

    video

--- single-mal-stop-sult.php
+++ single-mal-stop-sult.php
@@ -30,13 +30,14 @@
         </div> 
     </div>
 
+    <div class="bottom">
     <button> Forum </button>
 
     <div class="video">
 <iframe width="560" height="315" src="https://youtu.be/2UK5mY8b8gg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 </div>
-    
+</div>
     </div>
         
 
