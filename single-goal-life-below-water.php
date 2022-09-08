3f972398045fd311dfbcfa894eb1ed7882b04d85
jakub-stelzer <jakub.stelzer1@gmail.com>
2022-09-08 09:01:48 +0200

    Update single-goal-life-below-water.php

--- single-goal-life-below-water.php
+++ single-goal-life-below-water.php
@@ -457,14 +457,6 @@
                 </div>
             </div>
 
-
-            <div class="col-12">
-                <div class="col-6">
-                </div>
-                <div class="col-6">
-
-                </div>
-            </div>
 </div>
     </section> 
 
