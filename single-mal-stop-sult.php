f99ef420792dda7daee35d53d4fab5f36c749e33
mich163w <71692337+mich163w@users.noreply.github.com>
2022-09-08 12:01:08 +0200

    textbox

--- single-mal-stop-sult.php
+++ single-mal-stop-sult.php
@@ -6,32 +6,33 @@
     <h1 class="overskrift"> <?php the_title() ?></h1>
     
     <div class="col">
-    
-
-
-    
-      
-       <p class="tekst"><?php the_field("text") ?></p> 
+        <div class="box">
+         <p class="tekst"><?php the_field("text") ?></p> 
+       </div>
         <img class="image" src="<?php the_field("image") ?>" alt="">
         </div>
-        <div class="col">
+    <div class="col">
 
-    <img class="image" src="<?php the_field("image-2") ?>" alt="">
-    <p class="tekst"><?php the_field("text-2") ?></p> 
+        <img class="image" src="<?php the_field("image-2") ?>" alt="">
+        <div class="box">
+        <p class="tekst"><?php the_field("text-2") ?></p> 
+        </div> 
     </div>   
     
-    
     <div class="col">
-   
-    <p class="tekst"><?php the_field("text-3") ?></p> 
+    <div class="box">
+        <p class="tekst"><?php the_field("text-3") ?></p> 
+        </div> 
         <img class="image" src="<?php the_field("image-3") ?>" alt="">
     </div> 
 
     <div class="col">
-    <img class="image" src="<?php the_field("image-4") ?>" alt="">
-    <p class="tekst"><?php the_field("text-4") ?></p> 
-   
+        <img class="image" src="<?php the_field("image-4") ?>" alt="">
+        <div class="box">
+        <p class="tekst"><?php the_field("text-4") ?></p> 
+        </div> 
     </div>
+
     <button> Forum </button>
     
     </div>
