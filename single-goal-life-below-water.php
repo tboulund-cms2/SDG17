cf21a71b0de41c32e652f3b662f0e3d94bcbbc99
jakub-stelzer <jakub.stelzer1@gmail.com>
2022-09-08 09:06:39 +0200

    Update single-goal-life-below-water.php

--- single-goal-life-below-water.php
+++ single-goal-life-below-water.php
@@ -442,7 +442,7 @@
 
 
         <div class="newsletter-container">
-            <div class="col-12">
+            <div class="col-12 d-flex justifiy-content-center align-items-center">
                 <div class="col-6">
                     <h3><?php the_field("updated_text") ?></h3>
                 </div>
