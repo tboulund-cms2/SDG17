859e396a4410c6b2168ae94f8ca211a241c8ee26
Kporsman <95682845+Kporsman@users.noreply.github.com>
2022-09-08 20:28:02 +0200

    Update single-mal-mindre-ulighed.php

--- single-mal-mindre-ulighed.php
+++ single-mal-mindre-ulighed.php
@@ -9,7 +9,7 @@
 
        <section class="mid">
        <h2 class="overskrift2"><?php the_field("overskrift_2")?></h2>
-        < class="midmid">
+        <div class="midmid">
             <p class="text2"><?php the_field("text_2")?></p>
             <iframe width="560" height="315" src="<?php the_field("video") ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
